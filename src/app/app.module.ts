c20534a10e74b1dc3d8d59df41e4a2f2298c7594
jcp <jcp40500@gmail.com>
2023-12-22 18:04:22 +0100

    First stage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {CounterModule} from "./math/counter.module";
 import {HeroesModule} from "./heroes/heroes.module";
+import {DbzModule} from "./dbz/dbz.module";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {HeroesModule} from "./heroes/heroes.module";
     BrowserModule,
     BrowserAnimationsModule,
     CounterModule,
+    DbzModule,
     HeroesModule
   ],
   providers: [],
